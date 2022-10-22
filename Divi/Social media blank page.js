7cf61bb828a42384acb5bcb726525f242fe6cbae
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2022-10-22 14:54:27 +0200

    Create Social media blank page.js

---
+++ Divi/Social media blank page.js
@@ -0,0 +1,5 @@
+/* New window social media icon links => Toevoegen Divi => Integratie => Code */
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+	jQuery(".et-social-icon a").attr('target', 'blank');
+});
